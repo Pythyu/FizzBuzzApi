45293a6e3e9e4346b3eb0611ec8d50503fcf5806
Marethyu <antoine.zellmeyer@epita.fr>
2026-06-28 02:32:13 +0200

    feat(config): add support for default config removing the need of .env at build time, keeping it useful only for local dev

--- config/config.go
+++ config/config.go
@@ -7,19 +7,30 @@ import (
 	"github.com/joeshaw/envdecode"
 )
 
+const defaultServerPort = 8080
+const defaultServerWriteTimeout = 5
+const defaultServerReadTimeout = 3
+const defaultServerIdleTimeout = 5
+
 type ServerConfig struct {
-	Port         int           `env:"SERVER_PORT,required"`
-	TimeoutRead  time.Duration `env:"SERVER_TIMEOUT_READ,required"`
-	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE,required"`
-	TimeoutIdle  time.Duration `env:"SERVER_TIMEOUT_IDLE,required"`
-	Debug        bool          `env:"SERVER_DEBUG,required"`
+	Port         int           `env:"SERVER_PORT"`
+	TimeoutRead  time.Duration `env:"SERVER_TIMEOUT_READ"`
+	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE"`
+	TimeoutIdle  time.Duration `env:"SERVER_TIMEOUT_IDLE"`
+	Debug        bool          `env:"SERVER_DEBUG"`
 }
 
 func NewServerConfig() *ServerConfig {
-	var config ServerConfig
-	if err := envdecode.StrictDecode(&config); err != nil {
+	config := &ServerConfig{
+		Port:         defaultServerPort,
+		TimeoutRead:  defaultServerReadTimeout * time.Second,
+		TimeoutWrite: defaultServerWriteTimeout * time.Second,
+		TimeoutIdle:  defaultServerIdleTimeout * time.Second,
+		Debug:        false,
+	}
+	if err := envdecode.Decode(config); err != nil {
 		log.Fatalf("Failed to decode the server config: %s", err)
 	}
 
-	return &config
+	return config
 }
