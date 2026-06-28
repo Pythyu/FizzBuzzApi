a41598832f6f6f81af6356a54bd21916a166a247
Marethyu <antoine.zellmeyer@epita.fr>
2026-06-28 02:45:07 +0200

    fix(compose): add default for the server port

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 type ServerConfig struct {
-	Port         int           `env:"SERVER_PORT" envDefault:"8080"`
+	Port         int           `env:"SERVER_PORT" envDefault:"8989"`
 	TimeoutRead  time.Duration `env:"SERVER_TIMEOUT_READ" envDefault:"3s"`
 	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE" envDefault:"5s"`
 	TimeoutIdle  time.Duration `env:"SERVER_TIMEOUT_IDLE" envDefault:"5s"`
